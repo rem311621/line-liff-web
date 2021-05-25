b18fc79f3de8ad0a193b6dd2862d2e6e0d2217e6
rem311621 <rem311621@yahoo.com.tw>
2021-05-25 23:13:40 +0800

    My first commit

--- public/saveinfor.js
+++ public/saveinfor.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./test-line-bot-e06b0-firebase-adminsdk-ytkpy-a4ba4deeaa.json");
+const serviceAccount = require("../test-line-bot-e06b0-firebase-adminsdk-ytkpy-a4ba4deeaa.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });

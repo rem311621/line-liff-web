39bbc6f556b05d42a418a68d99b32a9e364cc4e2
rem311621 <rem311621@yahoo.com.tw>
2021-05-25 23:56:53 +0800

    My first commit

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  const db = require("../db");
   const useNodeJS = true; // if you are not using a node server, set this value to false
   const defaultLiffId = ""; // change the default LIFF value if you are not using a node server
 

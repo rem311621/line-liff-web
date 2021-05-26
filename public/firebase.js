a7672a63d49cea9ff4ab180eaf9939362d9d8573
rem311621 <rem311621@yahoo.com.tw>
2021-05-26 13:28:29 +0800

    My first commit

---
+++ public/firebase.js
@@ -0,0 +1,35 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0qxZKGrp3vSXzi_fizdtQDDEMOuO_ySg",
+  authDomain: "test-line-bot-e06b0.firebaseapp.com",
+  projectId: "test-line-bot-e06b0",
+  storageBucket: "test-line-bot-e06b0.appspot.com",
+  messagingSenderId: "671550270400",
+  appId: "1:671550270400:web:598b820aa5acbfc2d77625",
+  measurementId: "G-8E8MNE8R9X",
+};
+firebase.initializeApp(firebaseConfig);
+
+var firestore = firebase.firestore;
+const docRef = firestore.doc("inform/inputData");
+const savebtn = document.getElementById("savebtn");
+const input_name = document.getElementById("name").value;
+const input_addr = document.getElementById("addr").value;
+const input_phone = document.getElementById("phone").value;
+const input_pay = document.getElementById("pay").value;
+
+savebtn.addEventListener("click", function () {
+  console.log("save", inputname);
+  docRef
+    .set({
+      Name: input_name,
+      Addr: input_addr,
+      Phone: input_phone,
+      Pay: input_pay,
+    })
+    .then(() => {
+      console.log("saved");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
--- public/saveinfor.js
+++ public/saveinfor.js
@@ -1,4 +1,11 @@
-import { collection } from "../db";
+const admin = require("firebase-admin");
+const serviceAccount = require("./test-line-bot-e06b0-firebase-adminsdk-ytkpy-a4ba4deeaa.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true });
+//import { collection } from "../db";
 import Infor from "../models/inforModel";
 
 function test() {
@@ -11,8 +18,12 @@ const saveInfor = async (req, res, next) => {
     const phone = document.getElementById("phone").value;
     const pay = document.getElementById("pay").value;
     const data = req.body;
-    await collection("Information")
-      .add({ Name: 453, addr: 435, phone: 11, paymethod: 22 });
+    await collection("Information").add({
+      Name: 453,
+      addr: 435,
+      phone: 11,
+      paymethod: 22,
+    });
     res.send("Save Success");
   } catch (error) {
     res.status(400).send(error.message);
